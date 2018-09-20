34ccda5abb863b301d74e33d179c53421c07fcb3
Bowen <tbow1382@740s-pl-120.local>
2018-09-20 08:27:36 -0600

    imported model.Lifting and javax.swing.JOptionPane. Added Controller constructor, and JavaDoc

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,22 @@
 package controller;
 
+//Import Section
+import model.Lifting;
+import javax.swing.JOptionPane;
+
 public class Controller
 {
+	/**
+	 * Builds the instance of the Controller
+	 */
+	public Controller()
+	{
+		
+	}
 	
+	/**
+	 * This is where the program starts calling methods to run the program.
+	 */
 	public void start()
 	{
 		
