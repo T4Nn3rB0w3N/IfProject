316eaa26a8fb8aba1b5bb047d5e78e6e07d76321
Bowen <tbow1382@740s-pl-120.local>
2018-09-25 08:51:37 -0600

    added public boolean validDouble, and public boolean validInt; also added a "for" loop to private void loopy; and added private void askUser.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -33,10 +33,60 @@ public class Controller
 			JOptionPane.showMessageDialog(null,  "brother may I have some looops??");
 			//Eventually change the loop variable
 			count++;
-			if (count >= 10)
+			if (count >= 1)
 			{
 				isDone = true;  //update variable
 			}
-		}	
+		}
+		for (int loop = 0; loop < 30; loop += 2)
+		{
+			JOptionPane.showMessageDialog(null, "The loop value is: " + loop);
+		}
+	}
+	
+	private void askUser()
+	{
+		String response = JOptionPane.showInputDialog, "What is the distance???");
+		
+		while (!validInt(response))
+		{
+			response = JOptionPane.showInputDialog(null, "No! type in a valid number for distqnce");
+		}
+		
+		
+	}
+	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "This requires a double value aka something with a . >)");
+		}
+		
+		
+		return isValid;
+	}
+
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null,  "You should type a integer value like -12345");
+		}
+		
+		return isValid;
 	}
 }
