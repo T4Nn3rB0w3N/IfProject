bea69bc3717dd4239880222860673cb47b024c15
Bowen <tbow1382@740s-pl-120.local>
2018-09-25 08:36:02 -0600

    created a private void "while" loop.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -19,7 +19,24 @@ public class Controller
 	 */
 	public void start()
 	{
-		
+		loopy();
 	}
-
+	
+	private void loopy()
+	{
+		//Define variable before loop
+		boolean isDone = false;
+		int count = 0;
+		
+		while (!isDone)//Test the variable
+		{
+			JOptionPane.showMessageDialog(null,  "brother may I have some looops??");
+			//Eventually change the loop variable
+			count++;
+			if (count >= 10)
+			{
+				isDone = true;  //update variable
+			}
+		}	
+	}
 }
